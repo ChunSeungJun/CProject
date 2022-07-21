a79f0a7362c54d9f916e5b13c0a1a46d20ffcd40
CHUN <104182883+ChunSeungJun@users.noreply.github.com>
2022-07-21 16:07:53 +0900

    수업자료

--- CProject1/03_Literal/01_Literal.c
+++ CProject1/03_Literal/01_Literal.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-// ��� (Literal)
-// ������ �ʴ� ���� �ǹ��մϴ�.
+// 상수 (Literal)
+// 변하지 않는 값을 의미합니다.
 int main()
 {
 
@@ -14,9 +14,9 @@ int main()
 	A = 30;
 	A = 40;
 	
-	// ���ȭ const
-	// ��� B �� �����մϴ�.
-	// �ݵ�� ����� ���ÿ� �ʱ�ȭ�� ����Ǿ�� �մϴ�.
+	// 상수화 const
+	// 상수 B 를 선언합니다.
+	// 반드시 선언과 동시에 초기화가 진행되어야 합니다.
 	const int B = 20; 
 
 	
